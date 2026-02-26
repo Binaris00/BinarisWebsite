27a6d9cacb6ff9a8447015c30339fd7d90d16e9a
Binary <binaris000@gmail.com>
2026-02-25 21:43:08 -0400

    :bug: fix -> added EBR card + fix to weird card size when reaching 'canvas' limit

--- cards/start.md
+++ cards/start.md
@@ -2,5 +2,4 @@
 
 So... I don't know why you're here, this isn't finish yet.
 
-
 Hi! I'm Binaris and this is suppose to be my website. But I still didn't finish everything here sooo... Have some patience?
--- script.js
+++ script.js
@@ -12,64 +12,31 @@ let isPanning = false;
 let startX, startY;
 let canvasX = 0, canvasY = 0;
 
-cargarVariosArchivos();
+// base card (atm just the test init card)
+createCard('start', 300, 300);
+createCard('electroblobs wizardry redux', 400, 300);
 
-// base
-createCard('start');
-
-async function createCard(baseField) {
+/**
+ * Create a draggable card that will be added in the 600 - 300 of the screen
+ * @param {String} baseField 
+ * @param {int} left
+ * @param {int} top
+ */
+async function createCard(baseField, left, top) {
     const text = await fetch("/cards/" + baseField + ".md").then(r => r.text());
     var div = document.createElement('div');
     div.className = 'card';
     div.innerHTML = marked.parse(text);
-    div.style.left = '600px';
-    div.style.top = '300px';
+    div.style.left = left + 'px';
+    div.style.top = top + 'px';
     canvas.appendChild(div);
     makeCardDraggable(div);
 }
 
-// MOVER EL CANVAS (pan)
-canvasBackground.addEventListener('mousedown', (e) => {
-    // Solo si NO estamos sobre una card
-    if (e.target.closest('.card')) return;
-    
-    isPanning = true;
-    startX = e.clientX - canvasX;
-    startY = e.clientY - canvasY;
-    canvasBackground.style.cursor = 'grabbing';
-});
-
-async function cargarVariosArchivos() {
-  const archivos = [
-    '/cards/start.md',
-  ];
-  
-  const promesas = archivos.map(ruta => 
-    fetch(ruta).then(r => r.text())
-  );
-  
-  const contenidos = await Promise.all(promesas);
-  contenidos.forEach((contenido, index) => {
-    console.log(`Archivo ${archivos[index]}:`, contenido);
-  });
-}
-
-document.addEventListener('mousemove', (e) => {
-    if (!isPanning) return;
-    
-    canvasX = e.clientX - startX;
-    canvasY = e.clientY - startY;
-    canvas.style.transform = `translate(${canvasX}px, ${canvasY}px)`;
-    
-    canvasBackground.style.backgroundPosition = `${canvasX % 30}px ${canvasY % 30}px`;
-});
-
-document.addEventListener('mouseup', () => {
-    isPanning = false;
-    canvasBackground.style.cursor = 'grab';
-});
-
-// Función para hacer las cards arrastrables
+/**
+ * Add the needed event listeners to make the card draggable
+ * @param {Card} card 
+ */
 function makeCardDraggable(card) {
     let isDragging = false;
     let cardStartX, cardStartY;
@@ -98,3 +65,28 @@ function makeCardDraggable(card) {
         }
     });
 }
+
+// Move all the canvas
+canvasBackground.addEventListener('mousedown', (e) => {
+    if (e.target.closest('.card')) return;
+    
+    isPanning = true;
+    startX = e.clientX - canvasX;
+    startY = e.clientY - canvasY;
+    canvasBackground.style.cursor = 'grabbing';
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (!isPanning) return;
+    
+    canvasX = e.clientX - startX;
+    canvasY = e.clientY - startY;
+    canvas.style.transform = `translate(${canvasX}px, ${canvasY}px)`;
+    
+    canvasBackground.style.backgroundPosition = `${canvasX % 30}px ${canvasY % 30}px`;
+});
+
+document.addEventListener('mouseup', () => {
+    isPanning = false;
+    canvasBackground.style.cursor = 'grab';
+});
